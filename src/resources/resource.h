fe97786cf41961a6713f00ffcd3496cee5218db8
lollolong <65832054+lollolong@users.noreply.github.com>
2023-09-17 17:25:44 +0200

    add new icons
    Credits @OpenIV Team

--- src/resources/resource.h
+++ src/resources/resource.h
@@ -5,12 +5,14 @@
 #define IDI_RPF_ICON_BLUE               101
 #define IDI_RPF_ICON_GREEN              102
 #define IDI_SCRIPT_ICON                 103
+#define IDI_RESOURCE_ICON               104
+#define IDI_AWC_ICON                 	105
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
